90e08662267b506a3c80c2eb69c2591258a32fc7
Carlos Alberto López Garcés <16075585+CarlosSch@users.noreply.github.com>
2020-01-24 18:55:56 -0600

    correciones

--- table.js
+++ table.js
@@ -1,3 +1,17 @@
-$(document).ready( function () {
-    $('#example').DataTable();
-} );
+$(document).ready(function(){
+    $('#nomina').DataTable({
+       'processing': true,
+       'serverSide': true,
+       'serverMethod': 'post',
+       'ajax': {
+           'url':'test.php'
+       },
+       'columns': [
+          { data: 'emp_name' },
+          { data: 'email' },
+          { data: 'gender' },
+          { data: 'salary' },
+          { data: 'city' },
+       ]
+    });
+ });
--- test.php
+++ test.php
@@ -1,24 +1,17 @@
 <?php
     require 'include/conexion.php';  
     
-    $sql = "SELECT id_empleado,usuario, salario, CONCAT(nombre,' ',a_paterno,' ',a_materno) AS nombreC  FROM empleados ;";
+    $sql = "SELECT * FROM datos;";
     $result = $conn->query($sql);
 
 
     if ($result->num_rows > 0) {
         // output data of each row
         while($row = $result->fetch_assoc()) {
-            $sc = $row['salario'];
-            $name = $row['nombreC'];
-            $sb = ($sc/30) / 1.3;
-            $b1 = $sb * 1.1;
-            $b2 = $sb * 1.2;
-            $b3 = $sb * 1.3;
-
 
             $idata = array();
-            $idata[] = $row["id_empleado"];
-            $idata[] = $row["salario"];
+            $idata[] = $row["id_asistencias"];
+            $idata[] = $row["nombre"];
             $idata[] = $row["nombreC"];
             $idata[] = $row["usuario"];
 
